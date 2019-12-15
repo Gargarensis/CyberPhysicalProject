0b7fb2adf1a6bee4d9b8b2f7e092d9d52c8b278f
Riccardo Gabriele <gabririchi@yahoo.it>
2019-12-15 16:18:31 +0100

    Enable CORS

--- WebApp/server/app.js
+++ WebApp/server/app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const cors = require("cors");
 
 var indexRouter = require('./routes/index');
 var alarmRouter = require('./routes/alarm');
@@ -12,6 +13,9 @@ var app = express();
 // view engine setup
 // app.set('views', path.join(__dirname, 'views'));
 // app.set('view engine', 'jade');
+
+app.use(cors());
+app.options("*", cors());
 
 app.use(logger('dev'));
 app.use(express.json());

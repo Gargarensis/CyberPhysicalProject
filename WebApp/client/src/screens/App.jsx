67c6c901746acc7a5e30b95d1b27c1052bbd384c
gus <gustavo.graz@outlook.com>
2019-12-15 19:09:06 +0100

    Add alarm handling

--- WebApp/client/src/screens/App.jsx
+++ WebApp/client/src/screens/App.jsx
@@ -3,6 +3,7 @@ import React, { Component, Fragment } from 'react';
 import { Container, Grid } from 'semantic-ui-react'
 import 'semantic-ui-css/semantic.min.css'
 import axios from 'axios';
+import Request from 'axios-request-handler';
 
 import { AppHeader, SystemStatus, AlarmStatus } from "../components";
 
@@ -13,16 +14,32 @@ class App extends Component {
         super(props);
         
         this.state = {
-            sysStatus: false
+            sysStatus: false,
+            alStatus: false
         };
         
-        this.setSystemStatus = this.setSystemStatus.bind(this)
+        this.setSystemStatus = this.setSystemStatus.bind(this);
+        this.setAlarmStatus = this.setAlarmStatus.bind(this);
     }
     
     componentDidMount() {
-        axios.get("http://10.62.161.182:3001/alarm/enabled").then((res) => {
-            if(res.data && res.data !== this.state.status){
+        const system = new Request('http://10.62.161.182:3001/alarm/enabled');
+    
+        const alarm = new Request('http://10.62.161.182:3001/alarm');
+    
+        system.poll(5000).get((res) => {
+            if(res.hasOwnProperty("data") && res.data !== this.state.sysStatus){
                 this.setState({ sysStatus: res.data });
+            }
+            else if (res.status > 399) {
+                console.log( 'Error, please try again')
+            }
+        });
+    
+        alarm.poll(2000).get((res) => {
+            console.log(res.data)
+            if(res.hasOwnProperty("data") && res.data !== this.state.alStatus){
+                this.setState({ alStatus: res.data });
             }
             else if (res.status > 399) {
                 console.log( 'Error, please try again')
@@ -30,10 +47,20 @@ class App extends Component {
         });
     }
     
+    
+    
     setSystemStatus(event, data){
         axios.post(`http://10.62.161.182:3001/alarm/${data.checked?"enable":"disable"}`, {}).then((res) => {
             this.setState(this.setState({ sysStatus: data.checked }))
         });
+    }
+    
+    setAlarmStatus(){
+        if(this.state.sysStatus){
+            axios.post(`http://10.62.161.182:3001/alarm/${this.state.alStatus?"off":"on"}`, {}).then((res) => {
+                this.setState(this.setState({ alStatus: !this.state.alStatus }))
+            });
+        }
     }
     
     render() {
@@ -48,8 +75,9 @@ class App extends Component {
                                 <AppHeader/>
                                 <SystemStatus status={this.state.sysStatus}
                                               setStatus={this.setSystemStatus}/>
-                                <AlarmStatus status={this.state.sysStatus}
-                                              setStatus={this.setSystemStatus}/>
+                                <AlarmStatus status={this.state.alStatus}
+                                             sysStatus={this.state.sysStatus}
+                                             setStatus={this.setAlarmStatus}/>
                             </Container>
                         </Grid.Column>
                     </Grid>
